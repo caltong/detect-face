3b3be60c7499ed2d9b199f24b6e16d0260488279
caltong <caltong1027@gmail.com>
2019-09-06 17:16:36 +0800

    VGG16 预训练权重

--- model.py
+++ model.py
@@ -1,3 +1,22 @@
-import keras as K
+from keras.applications.vgg16 import VGG16
+from keras.layers import Dense, AveragePooling2D
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
 import numpy as np
-import tensorflow as tf
+
+base_model = VGG16(weights='imagenet', include_top=False)
+
+x = base_model.output
+x = AveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(4)(x)
+
+
+img_path = 'data/data_covered/0.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+features = model.predict(x)
+print(features)
