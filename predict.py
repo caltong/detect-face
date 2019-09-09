35f21e204e8b65e51f13c50885c07cdeaf50d5dd
caltong <caltong1027@gmail.com>
2019-09-09 16:55:51 +0800

    测试模型

--- predict.py
+++ predict.py
@@ -1,10 +1,16 @@
 import keras
 from PIL import Image
 import numpy as np
+from keras.backend import backend as K
+
+
+def loss(y_true, y_pred):
+    return K.mean((K.square(y_true - y_pred)))
+
 
 model = keras.models.load_model('model_use_vgg16.h5')
 
-image = Image.open('data/data_test/2.png-photo0.png')
+image = Image.open('data/data_covered/00000.jpg')
 image = image.resize((224, 224), Image.ANTIALIAS)
 pixel = np.asarray(image)
 pixel = np.expand_dims(pixel, axis=0)
