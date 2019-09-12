8ba63b23aa51c63c52d00929a407f099451e963a
caltong <caltong1027@gmail.com>
2019-09-12 12:58:32 +0800

    修改损失函数

--- model_step_by_step.py
+++ model_step_by_step.py
@@ -10,8 +10,8 @@ from data_augmentation import make_data_set
 # YOLO_v1 中x,y,w,h 损失函数
 def loss(y_true, y_pred):
     a = K.square(y_pred[:, 0] - y_true[:, 0]) + K.square(y_pred[:, 1] - y_true[:, 1])
-    b = K.square(K.sqrt(K.abs(y_pred[:, 2])) - K.sqrt(K.abs(y_true[:, 2]))) + K.square(
-        K.sqrt(K.abs(y_pred[:, 3])) - K.sqrt(K.abs(y_true[:, 3])))
+    b = K.square(K.abs(y_pred[:, 2]) - K.sqrt(y_true[:, 2])) + K.square(
+        K.sqrt(y_pred[:, 3]) - K.sqrt(y_true[:, 3]))
     value = a + b
     return value
 
