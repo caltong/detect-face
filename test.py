3557e83c973320597f9f5baba9e3273186d23bdc
caltong <caltong1027@gmail.com>
2019-09-18 10:04:44 +0800

    debug

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ while True:
     image = Image.open(path)
     image_resize = image.resize((224, 224))
     draw = ImageDraw.Draw(image_resize)
-    preditc = input('Predict: ')
-    preditc = preditc.split(' ')
-    for i in range(len(preditc)):
-        preditc[i] = float(preditc[i])
+    predict = input('Predict: ')
+    predict = predict.split(' ')
+    for i in range(len(predict)):
+        predict[i] = float(predict[i])
 
-    x, y, w, h = preditc[0] * 224, preditc[1] * 224, preditc[2] * 224, preditc[3] * 224
+    x, y, w, h = predict[0] * 224, predict[1] * 224, predict[2] * 224, predict[3] * 224
     draw.rectangle([x - w / 2, y - h / 2, x + w / 2, y + h / 2], width=5)
     image_resize.show()
