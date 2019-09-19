19357ce8bd479649c6bc9fa95ff559587062e184
caltong <caltong1027@gmail.com>
2019-09-19 10:16:34 +0800

    修改损失函数

--- model_step_by_step.py
+++ model_step_by_step.py
@@ -8,16 +8,16 @@ from data_augmentation import make_data_set
 
 
 # YOLO_v1 中x,y,w,h 损失函数
-# def loss(y_true, y_pred):
-#     a = K.square(y_pred[:, 0] - y_true[:, 0]) + K.square(y_pred[:, 1] - y_true[:, 1])
-#     b = K.square(K.sqrt(y_pred[:, 2]) - K.sqrt(y_true[:, 2])) + K.square(
-#         K.sqrt(y_pred[:, 3]) - K.sqrt(y_true[:, 3]))
-#     value = a + b
-#     return value
+def loss(y_true, y_pred):
+    a = K.square(y_pred[:, 0] - y_true[:, 0]) + K.square(y_pred[:, 1] - y_true[:, 1])
+    b = K.square(K.sqrt(y_pred[:, 2]) - K.sqrt(y_true[:, 2])) + K.square(
+        K.sqrt(y_pred[:, 3]) - K.sqrt(y_true[:, 3]))
+    value = a + b
+    return value
 
 
-# model = keras.models.load_model('model_use_vgg16.h5', custom_objects={'loss': loss})
-model = keras.models.load_model('model_use_vgg16.h5')
+model = keras.models.load_model('model_use_vgg16.h5', custom_objects={'loss': loss})
+# model = keras.models.load_model('model_use_vgg16.h5')
 # 不同数据
 for i in range(1600):
     print('Round: ' + str(i))
--- predict.py
+++ predict.py
@@ -4,16 +4,16 @@ import numpy as np
 from keras import backend as K
 
 # YOLO_v1 中x,y,w,h 损失函数
-# def loss(y_true, y_pred):
-#     a = K.square(y_pred[:, 0] - y_true[:, 0]) + K.square(y_pred[:, 1] - y_true[:, 1])
-#     b = K.square(K.sqrt(y_pred[:, 2]) - K.sqrt(y_true[:, 2])) + K.square(
-#         K.sqrt(y_pred[:, 3]) - K.sqrt(y_true[:, 3]))
-#     value = a + b
-#     return value
+def loss(y_true, y_pred):
+    a = K.square(y_pred[:, 0] - y_true[:, 0]) + K.square(y_pred[:, 1] - y_true[:, 1])
+    b = K.square(K.sqrt(y_pred[:, 2]) - K.sqrt(y_true[:, 2])) + K.square(
+        K.sqrt(y_pred[:, 3]) - K.sqrt(y_true[:, 3]))
+    value = a + b
+    return value
 
 
-# model = keras.models.load_model('model_20190918.h5', custom_objects={'loss': loss})
-model = keras.models.load_model('model_20190918.h5')
+model = keras.models.load_model('model_20190918.h5', custom_objects={'loss': loss})
+# model = keras.models.load_model('model_20190918.h5')
 output = []
 while True:
     print('input image path:')
