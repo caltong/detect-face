1aada0b74c6b73dc7b4ebe88d551adf5187093a9
caltong <caltong1027@gmail.com>
2019-09-19 10:27:51 +0800

    使用face++api

---
+++ faceplusplus_api.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import urllib.request
+import urllib.error
+import time
+import json
+from PIL import Image, ImageDraw
+from api_key import http_url, key, secret
+
+
+def faceplusplus_api(filepath):
+    filepath = filepath
+
+    boundary = '----------%s' % hex(int(time.time() * 1000))
+    data = []
+    data.append('--%s' % boundary)
+    data.append('Content-Disposition: form-data; name="%s"\r\n' % 'api_key')
+    data.append(key)
+    data.append('--%s' % boundary)
+    data.append('Content-Disposition: form-data; name="%s"\r\n' % 'api_secret')
+    data.append(secret)
+    data.append('--%s' % boundary)
+    fr = open(filepath, 'rb')
+    data.append('Content-Disposition: form-data; name="%s"; filename=" "' % 'image_file')
+    data.append('Content-Type: %s\r\n' % 'application/octet-stream')
+    data.append(fr.read())
+    fr.close()
+    data.append('--%s' % boundary)
+    data.append('Content-Disposition: form-data; name="%s"\r\n' % 'return_landmark')
+    data.append('1')
+    data.append('--%s' % boundary)
+    data.append('Content-Disposition: form-data; name="%s"\r\n' % 'return_attributes')
+    data.append(
+        "gender,age,smiling,headpose,facequality,blur,eyestatus,emotion,ethnicity,beauty,mouthstatus,eyegaze,skinstatus")
+    data.append('--%s--\r\n' % boundary)
+
+    for i, d in enumerate(data):
+        if isinstance(d, str):
+            data[i] = d.encode('utf-8')
+
+    http_body = b'\r\n'.join(data)
+
+    # build http request
+    req = urllib.request.Request(url=http_url, data=http_body)
+
+    # header
+    req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
+
+    try:
+        # post data to server
+        resp = urllib.request.urlopen(req, timeout=5)
+        # get response
+        qrcont = resp.read()
+        # if you want to load as json, you should decode first,
+        # for example: json.loads(qrount.decode('utf-8'))
+        print(qrcont.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(e.read().decode('utf-8'))
+
+    data = json.loads(qrcont.decode('utf-8'))
+    left_eye = [data['faces'][0]['landmark']['left_eye_center']['x'],
+                data['faces'][0]['landmark']['left_eye_center']['y']]
+    right_eye = [data['faces'][0]['landmark']['right_eye_center']['x'],
+                 data['faces'][0]['landmark']['right_eye_center']['y']]
+
+    image = Image.open(filepath)
+    draw = ImageDraw.Draw(image)
+    draw.ellipse([left_eye[0] - 3, left_eye[1] - 3, left_eye[0] + 3, left_eye[1] + 3], fill='red')
+    draw.ellipse([right_eye[0] - 3, right_eye[1] - 3, right_eye[0] + 3, right_eye[1] + 3], fill='red')
+    image.show()
