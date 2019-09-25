c50d4cacb5e8a146d118e6d1406416f79619f18a
caltong <caltong1027@gmail.com>
2019-09-25 15:33:12 +0800

    修改 loss function

--- model_use_vgg16_fine_tuning.py
+++ model_use_vgg16_fine_tuning.py
@@ -22,9 +22,8 @@ for layer in base_model.layers:
 
 # YOLO_v1 中x,y,w,h 损失函数
 def loss(y_true, y_pred):
-    a = K.square(y_pred[:, 0] - y_true[:, 0]) + K.square(y_pred[:, 1] - y_true[:, 1])
-    b = K.square(K.sqrt(y_pred[:, 2]) - K.sqrt(y_true[:, 2])) + K.square(
-        K.sqrt(y_pred[:, 3]) - K.sqrt(y_true[:, 3]))
+    a = K.abs(y_pred[:, 0] - y_true[:, 0]) + K.abs(y_pred[:, 1] - y_true[:, 1])
+    b = K.abs(K.sqrt(y_pred[:, 2]) - K.sqrt(y_true[:, 2])) + K.abs(K.sqrt(y_pred[:, 3]) - K.sqrt(y_true[:, 3]))
     value = a + b
     return value
 
@@ -34,26 +33,26 @@ model.compile(optimizer='rmsprop', loss=loss)
 # fine tuning 全连接层
 x_train, y_train = load_data()
 print(x_train.shape, y_train.shape)
-model.fit(x_train, y_train, epochs=1, batch_size=16, verbose=1)
+model.fit(x_train, y_train, epochs=2, batch_size=16, verbose=1)
 
 # fine tuning 第二次
-make_data_set()
-x_train, y_train = load_data()
+# make_data_set()
+# x_train, y_train = load_data()
 for layer in model.layers[:11]:
-    layer.trainable = False
+    layer.trainable = True
 for layer in model.layers[11:]:
     layer.trainable = True
 model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss=loss)
 model.fit(x_train, y_train, epochs=4, batch_size=16, verbose=1)
 
 # fine tuning 第三次
-make_data_set()
-x_train, y_train = load_data()
-for layer in model.layers[:5]:
-    layer.trainable = False
-for layer in model.layers[5:]:
-    layer.trainable = True
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss=loss)
-model.fit(x_train, y_train, epochs=4, batch_size=16, verbose=1)
+# make_data_set()
+# x_train, y_train = load_data()
+# for layer in model.layers[:5]:
+#     layer.trainable = False
+# for layer in model.layers[5:]:
+#     layer.trainable = True
+# model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss=loss)
+# model.fit(x_train, y_train, epochs=4, batch_size=16, verbose=1)
 
 model.save('model_use_vgg16_fine_tuning.h5')
