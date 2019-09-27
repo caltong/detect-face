210ee260a81eb6f8cc96b732a84635295d38329b
caltong <caltong1027@gmail.com>
2019-09-28 01:01:23 +0800

    取消动态使用显存

--- model_use_vgg16_fine_tuning.py
+++ model_use_vgg16_fine_tuning.py
@@ -1,4 +1,4 @@
-from keras.applications import VGG16
+from keras.applications import VGG16, InceptionV3
 from keras.utils import plot_model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.models import Model
@@ -6,17 +6,18 @@ from load_data import load_data
 from keras import backend as K
 from data_augmentation import make_data_set
 from keras.optimizers import SGD
+from keras import losses
 
 from keras import backend as K
 
 # set GPU memory
-# if ('tensorflow' == K.backend()):
-#     import tensorflow as tf
-#     from keras.backend.tensorflow_backend import set_session
-#
-#     config = tf.ConfigProto()
-#     config.gpu_options.allow_growth = True
-#     sess = tf.Session(config=config)
+if ('tensorflow' == K.backend()):
+    import tensorflow as tf
+    from keras.backend.tensorflow_backend import set_session
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
 
 base_model = VGG16(weights='imagenet', include_top=False)
 
@@ -35,7 +36,7 @@ for layer in base_model.layers:
 def loss(y_true, y_pred):
     a = K.abs(y_pred[:, 0] - y_true[:, 0]) + K.abs(y_pred[:, 1] - y_true[:, 1])
     b = K.abs(K.sqrt(y_pred[:, 2]) - K.sqrt(y_true[:, 2])) + K.abs(K.sqrt(y_pred[:, 3]) - K.sqrt(y_true[:, 3]))
-    value = a + b
+    value = K.mean(a + b, axis=-1)
     return value
 
 
@@ -49,9 +50,11 @@ model.fit(x_train, y_train, epochs=2, batch_size=16, verbose=1)
 # fine tuning 第二次
 # make_data_set()
 # x_train, y_train = load_data()
-for layer in model.layers[:11]:
-    layer.trainable = True
-for layer in model.layers[11:]:
+# for layer in model.layers[:11]:
+#     layer.trainable = True
+# for layer in model.layers[11:]:
+#     layer.trainable = True
+for layer in model.layers:
     layer.trainable = True
 model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss=loss)
 model.fit(x_train, y_train, epochs=4, batch_size=16, verbose=1)
